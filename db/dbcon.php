4afa3013300ed10f67990aafc66ecb1ca6c9342f
Md Jasim Uddin <mdjasimh944@gmail.com>
2025-07-21 01:29:47 +0600

    first deploy code in server just change database connection

--- db/dbcon.php
+++ db/dbcon.php
@@ -1,8 +1,13 @@
 <?php
-$host = "127.0.0.1";
-$dbUser = "root";
-$dbPwd = "";
-$dbName  = "cost_counter";
+// $host = "127.0.0.1";
+// $dbUser = "root";
+// $dbPwd = "";
+// $dbName  = "cost_counter";
+
+$host =  "localhost";
+$dbUser = "amarsite";
+$dbPwd = "7[Vq3B7NeJp4g;a";
+$dbName  = "amarsite_cost_counter";
 
 $con = mysqli_connect($host, $dbUser, $dbPwd, $dbName);
 if(!$con){
