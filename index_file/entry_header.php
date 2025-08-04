b047159cf1d19107c7c8e6d4af8f5ce9b1959f11
Md Jasim Uddin <mdjasimh944@gmail.com>
2025-08-04 20:33:10 +0600

    version-0.01 media query add for entry_header and header_nav mobile responsive

--- index_file/entry_header.php
+++ index_file/entry_header.php
@@ -1,4 +1,4 @@
- <div class="d-flex justify-content-between align-items-center mb-3">
+ <div class="d-flex justify-content-between align-items-center mb-3 entry-header">
   
   <!-- Selected Month Badge -->
   <h5 class="mb-0">
--- index_file/header.php
+++ index_file/header.php
@@ -5,9 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ডেইলি খরচ এন্ট্রি</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <style>
+  <link rel="stylesheet" href="css/index.css">
+
+  <!-- <style>
     .date-card { background: #f8f9fa; border-left: 5px solid orange; padding: 10px; margin-bottom: 10px; }
     .cost-entry { padding-left: 20px; }
-  </style>
+  </style> -->
+
+
 </head>
 <body>
